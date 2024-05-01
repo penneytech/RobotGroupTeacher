257a439f6a5cce6379087ac3f382213ae03a94ef
paulpenney <paul.penney@gmail.com>
2024-05-01 11:08:10 -0400

    Create RobotGroupTeacher.ino

---
+++ RobotGroupTeacher/RobotGroupTeacher.ino
@@ -0,0 +1,15 @@
+
+#define IR_1 2
+#define IR_2 4
+#define IR_3 7
+#define IR_4 8
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
